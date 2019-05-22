6bb436680815ba26ca8ea6e8ef77c5013b11df07
DougW <ddwilli@gmail.com>
2019-05-22 10:11:38 -0400

    add simple downloader

--- src/config_local.py
+++ src/config_local.py
@@ -1,3 +1,9 @@
 def test():
-    print('Hi There')
+    print('Low There')
 
+def get_github_files(flist, prefix = 'https://raw.githubusercontent.com/williamsdoug/CTG_RP/master/src/'):
+    import urllib.request
+
+    for fname in flist:
+        print('Beginning file download of file', fname)
+        urllib.request.urlretrieve(prefix+fname, fname)
