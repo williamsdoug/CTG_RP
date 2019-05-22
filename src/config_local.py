5d0c90ed81eb84c285b328a81bf4172a4420a026
DougW <ddwilli@gmail.com>
2019-05-22 10:24:34 -0400

    Update config_local.py

--- src/config_local.py
+++ src/config_local.py
@@ -1,15 +1,19 @@
-def test():
-    print('Low There')
+GITHUB_PREFIX = 'https://raw.githubusercontent.com/williamsdoug/CTG_RP/master/src/'
+GITHUB_DEFAULT_SRC_FILES = ['test.py']
 
-def get_github_files(flist, prefix = 'https://raw.githubusercontent.com/williamsdoug/CTG_RP/master/src/'):
+def get_github_files(flist):
     import urllib.request
     import os
 
     for fname in flist:
         try:
             print('Beginning file download of file', fname)
-            os.remove(fname)
-            urllib.request.urlretrieve(prefix+fname, fname)
+            if '*' not in fname and '/' not in fname and '?' not in fname:
+                os.remove(fname)
+                urllib.request.urlretrieve(GITHUB_PREFIX+fname, fname)
         except:
             print('Download failed for file', fname)
     print('Done')
+
+def get_default_github_src_files():
+    get_github_files(GITHUB_DEFAULT_SRC_FILES)
